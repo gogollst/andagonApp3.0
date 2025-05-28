055575fd45f21323d1f9ba9e48c5a67e887c03f8
gogollst <s.gogoll@andagon.com>
2025-05-28 12:04:45 +0200

    Add role support

--- andagonApp3/Data/ApplicationUser.cs
+++ andagonApp3/Data/ApplicationUser.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace andagonApp3.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public List<string> Roles { get; set; } = new();
     }
 
 }
