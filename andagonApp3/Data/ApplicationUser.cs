ba03923cb69d391d232f9f97d00424b0ae329be3
gogollst <s.gogoll@andagon.com>
2025-05-28 14:47:31 +0200

    Assign default user role

--- andagonApp3/Data/ApplicationUser.cs
+++ andagonApp3/Data/ApplicationUser.cs
@@ -6,7 +6,7 @@ namespace andagonApp3.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        public List<string> Roles { get; set; } = new();
+        public List<string> Roles { get; set; } = new() { ApplicationRoles.User };
     }
 
 }
